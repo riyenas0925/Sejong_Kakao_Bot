96231b1d86a980d095e09f5ba8b48aca4d0b7a4d
riyenas0925 <riyenas0925@gmail.com>
2020-06-07 13:51:03 +0900

    ENH :  사용자의 요청에 따라 공지사항을  분류별로 보여주는 기능 구현
    * NoticeTypeEnum을 통해 세종대학교 공지사항 주소의  파라미터를 반환
    * 사용자의 발화(일반, 장학, 입학 등등)에 따라 해당 공지사항을 표시
    * 공지사항의 등록일자가 보이도록 추가

---
+++ src/main/java/dev/riyenas/chatbot/service/NoticeTypeEnum.java
@@ -0,0 +1,35 @@
+package dev.riyenas.chatbot.service;
+
+import java.util.Arrays;
+
+public enum NoticeTypeEnum {
+    NORMAL("일반", "333"),
+    ENTRANCE("입학", "334"),
+    SCHOOL("학사", "335"),
+    JOB("취업", "337"),
+    SCHOLARSHIP("장학", "338"),
+    RECRUITMENT("교내모집", "339");
+
+    private String title;
+    private String fk;
+
+    NoticeTypeEnum(String title, String fk){
+        this.title = title;
+        this.fk = fk;
+    }
+
+    public static NoticeTypeEnum findByTitle(String title){
+        return Arrays.stream(NoticeTypeEnum.values())
+                .filter(noticeType -> noticeType.getTitle().equals(title))
+                .findAny()
+                .orElse(NoticeTypeEnum.NORMAL);
+    }
+
+    public String getFk() {
+        return fk;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+}
--- src/main/java/dev/riyenas/chatbot/web/KakaoSkillApi.java
+++ src/main/java/dev/riyenas/chatbot/web/KakaoSkillApi.java
@@ -1,17 +1,21 @@
 package dev.riyenas.chatbot.web;
 
+import dev.riyenas.chatbot.domain.SkillPayload;
 import dev.riyenas.chatbot.service.NoticeCrawlerService;
+import dev.riyenas.chatbot.service.NoticeTypeEnum;
 import dev.riyenas.chatbot.web.dto.common.QuickReplyEnum;
 import dev.riyenas.chatbot.domain.SkillResponse;
 import dev.riyenas.chatbot.domain.SkillResponseTemplate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Map;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -22,16 +26,22 @@ public class KakaoSkillApi {
     private final NoticeCrawlerService noticeCrawlerService;
 
     @PostMapping("notice")
-    public SkillResponse sejongNotice() throws IOException {
+    public SkillResponse sejongNotice(@RequestBody SkillPayload payload) throws IOException {
+
+        Map<String, String> params = (Map<String, String>) payload.getAction().get("params");
+        String noticeTitle = params.get("sys_sejong_notice_type");
+
+        log.info(noticeTitle + " : " + payload.toString());
+
         return new SkillResponseTemplate()
-                .addSimpleText("세종대학교 공지사항 입니다.")
-                .addListCard(noticeCrawlerService.sejongNoticeCrawler())
+                .addSimpleText("세종대학교 " + noticeTitle + " 공지사항 입니다.")
+                .addListCard(noticeCrawlerService.sejongNoticeCrawler(NoticeTypeEnum.findByTitle(noticeTitle)))
                 .addQuickReplies(
                         Arrays.asList(
-                                QuickReplyEnum.MESSAGE.action("일반공지", "일반공지 공지사항 알려줘"),
+                                QuickReplyEnum.MESSAGE.action("일반", "일반 공지사항 알려줘"),
                                 QuickReplyEnum.MESSAGE.action("입학", "입학 공지사항 알려줘"),
                                 QuickReplyEnum.MESSAGE.action("학사", "학사 공지사항 알려줘"),
-                                QuickReplyEnum.MESSAGE.action("국제교류", "국제교류 공지사항 알려줘"),
+                                //QuickReplyEnum.MESSAGE.action("국제교류", "국제교류 공지사항 알려줘"),
                                 QuickReplyEnum.MESSAGE.action("취업", "취업 공지사항 알려줘"),
                                 QuickReplyEnum.MESSAGE.action("장학", "장학 공지사항 알려줘"),
                                 QuickReplyEnum.MESSAGE.action("교내모집", "교내모집 공지사항 알려줘")
